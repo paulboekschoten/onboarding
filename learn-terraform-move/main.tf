cdb8c27a79dec7f7b1c7b9262085b66b051c2bc6
Paul Boekschoten <paul.boekschoten@hashicorp.com>
2022-04-26 13:51:43 +0200

    move resources

---
+++ learn-terraform-move/main.tf
@@ -0,0 +1,115 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "learn_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.21.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+
+  tags = var.vpc_tags
+}
+
+module "ec2_instance" {
+  source          = "./modules/compute"
+  security_group = module.security_group.sg_id
+  public_subnets  = module.learn_vpc.public_subnets
+}
+
+module "security_group" {
+  source = "./modules/security_group"
+  vpc_id    = module.learn_vpc.vpc_id
+}
+
+moved {
+  from = aws_instance.example
+  to = module.ec2_instance.aws_instance.example
+}
+
+moved {
+  from = aws_security_group.sg_8080
+  to = module.security_group.aws_security_group.sg_8080
+}
+
+moved {
+  from = module.vpc
+  to   = module.learn_vpc
+}
+
+
+/*
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_instance" "example" {
+  ami                    = data.aws_ami.ubuntu.id
+  subnet_id              = module.vpc.public_subnets[0]
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sg_8080.id]
+  user_data              = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+              echo "Hello World" > /var/www/html/index.html
+              systemctl restart apache2
+              EOF
+  tags = {
+    Name = "terraform-learn-move-ec2"
+  }
+}
+
+resource "aws_security_group" "sg_8080" {
+  vpc_id = module.vpc.vpc_id
+  name   = "terraform-learn-move-sg"
+  ingress {
+    from_port   = "8080"
+    to_port     = "8080"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+*/
