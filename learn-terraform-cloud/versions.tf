e30c47da7866ae95a7563c0fa2ecbf13043a7b2b
Paul Boekschoten <paul.boekschoten@hashicorp.com>
2022-04-28 14:22:54 +0200

    learn cloud

---
+++ learn-terraform-cloud/versions.tf
@@ -0,0 +1,19 @@
+terraform {
+
+  cloud {
+    organization = "paulboekschoten-business"
+
+    workspaces {
+      name = "learn-terraform-cloud"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.11.0"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
